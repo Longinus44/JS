2059da32f1275ee9c813b3c61bfd5058bc66186c
Longinus44 <chijiokelonginus4@gmail.com>
2022-04-21 12:53:14 +0100

    first commit

---
+++ indexhh.js
@@ -0,0 +1,35 @@
+let school = [
+    {
+        teacher: ['james', 'Steve', 'debby', 'douglas'],
+        students: {
+            class1: ['hate', 'likes', 'shadow', 'enter'],
+            class2: ['solve', 'get', 'doe', 'gentle'],
+            class3: ['went', 'down', 'sing', 'sang'],
+            class4: ['dawn', 'watch', 'ding', 'fine'],
+
+        }
+    },
+    {
+        cleaners: ['pen', 'pencil', 'elegant', 'beauty'],
+        subjects: ['chemistry', 'biology', 'maths', 'geology'],
+    },
+    {
+        management: {
+            principal: 'jones',
+            'vice principal': 'mary',
+            'board of truustess': 'chuks'
+        }
+    }
+]
+    console.log(school[0].teacher[3]);
+    console.log(school[1].subjects[2]);
+    console.log(school[0].students.class2[2]);
+    console.log(school[2].management["board of truustess"]);
+    
+
+// // things to get in your console
+// douglas
+// maths
+// doe
+// chuks 
+
