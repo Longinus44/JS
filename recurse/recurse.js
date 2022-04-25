9a63c5c705ad402010983142daf72dacf9b41228
Longinus44 <chijiokelonginus4@gmail.com>
2022-04-25 22:55:57 +0000

    tenary funct added

--- recurse/recurse.js
+++ recurse/recurse.js
@@ -10,3 +10,10 @@ console.log(fact(3));
 console.log(fact(2));
 console.log(fact(1));
 
+function fact (x){
+    return(x == 0) ? 1 :(x * fact(x-1))
+}  console.log(fact(5));
+console.log(fact(4));
+console.log(fact(3));
+console.log(fact(2));
+console.log(fact(1));
