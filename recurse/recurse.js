9a1774aff80d7e1b2c6abcb63aa41e704643579d
Longinus44 <chijiokelonginus4@gmail.com>
2022-04-25 22:29:18 +0000

    recursion task

---
+++ recurse/recurse.js
@@ -0,0 +1,12 @@
+function fact(x) {
+    if (x == 0) {
+        return 1;
+    } else {
+        return x * fact(x - 1)
+    }
+} console.log(fact(5));
+console.log(fact(4));
+console.log(fact(3));
+console.log(fact(2));
+console.log(fact(1));
+
