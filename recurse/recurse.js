228ba1b3f9625fa639882472080a8be4b83f6b41
Longinus44 <chijiokelonginus4@gmail.com>
2022-06-25 16:06:47 +0000

    just done

--- recurse/recurse.js
+++ recurse/recurse.js
@@ -17,3 +17,13 @@ console.log(fact(4));
 console.log(fact(3));
 console.log(fact(2));
 console.log(fact(1));
+
+
+
+function fib(n){
+    if(n < 2){
+        return 1;
+    }else{
+        return n + fib(n-1) + fib(n - 2)
+    }
+}console.log(5);2
